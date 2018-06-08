cf21bad2e60bc41ada2f6a476de82c35bf90a9d5
gaoqiang19514 <gaoqiang19514@163.com>
2018-06-08 23:06:28 +0800

    update

--- src/pages/login/views/login.js
+++ src/pages/login/views/login.js
@@ -33,47 +33,41 @@ class Login extends Component {
             }
         }
 
-        var md = forge.md.md5.create();
-        var replaceAll = function(str, sptr, sptr1){
-            while (str.indexOf(sptr) >= 0){
-                str = str.replace(sptr, sptr1);
-            }
-            return str;
+        const APP_KAY = '9wsez1o5cc2oetj6f6n8oh'
+        
+        const signature = () => {
+            let signParams = [
+                'ver=5.1.0',
+                'uuid=915EFFC9-BCA6-4431-A36A-D4B5069D36D3',
+                'systemVersion=11.2',
+                'page_size=10',
+                'page_no=1',
+                'openid=p2p_ios',
+                '_type=json'
+            ];
+    
+            signParams = signParams.sort()
+    
+            let signParamsString = signParams.join('&');
+            let signParamsAndAppkey = signParamsString + APP_KAY;
+    
+            const md = forge.md.md5.create();
+            md.update(signParamsAndAppkey);
+            const sign = md.digest().toHex();
+    
+            return 'sign='+ sign + '&' + 'sign_type=' + 'MD5&' + signParamsString;
         }
 
-        var _sign = [
-            'ver=5.1.0',
-            'uuid=915EFFC9-BCA6-4431-A36A-D4B5069D36D3',
-            'systemVersion=11.2',
-            'page_size=10',
-            'page_no=1',
-            'openid=p2p_ios',
-            '_type=json'
-        ];
+        const keyStr = signature()
 
-        let _result = _sign.sort();
-
-        const appkey = ''
-
-        let result2 = _result.toString();
-        let result = result2.replace(',', '&') + appkey;
-
-        result2 = replaceAll(result2, ',', '&');
-        result = replaceAll(result, ',', '&');
-
-        md.update(result);
-        const key = md.digest().toHex();
-        const keyStr = 'sign='+key + '&' + 'sign_type=' + 'MD5&' + result2
 
         axios.get('/product/activity_list?' + keyStr)
         .then(function (response) {
             console.log(response);
-
         })
         .catch(function (error) {
 
         });
-
         
         this.login             = this.login.bind(this)
         this.handleSubmit      = this.handleSubmit.bind(this)
@@ -81,10 +75,7 @@ class Login extends Component {
     }
 
     login(token) {
-        // const openid = 'p2p_ios
-        // const sign_type = 'MD5'
-
-        // this.props.onLogin(token)
+        this.props.onLogin(token)
     }
 
     handleValueChange(field, value, type = 'string') {
