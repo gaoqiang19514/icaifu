b0d3fc1889cd71e2f9f8deaa5bd91a51c3aa77c5
gaoqiang19514 <gaoqiang19514@163.com>
2018-06-11 09:42:35 +0800

    update

--- src/pages/login/views/login.js
+++ src/pages/login/views/login.js
@@ -31,42 +31,6 @@ class Login extends Component {
                 }
             }
         }
-
-        const APP_KAY = '9wsez1o5cc2oetj6f6n8oh'
-        
-        const signature = () => {
-            let signParams = [
-                'ver=5.1.0',
-                'uuid=915EFFC9-BCA6-4431-A36A-D4B5069D36D3',
-                'systemVersion=11.2',
-                'page_size=10',
-                'page_no=1',
-                'openid=p2p_ios',
-                '_type=json'
-            ];
-    
-            signParams = signParams.sort()
-    
-            let signParamsString = signParams.join('&');
-            let signParamsAndAppkey = signParamsString + APP_KAY;
-    
-            const md = forge.md.md5.create();
-            md.update(signParamsAndAppkey);
-            const sign = md.digest().toHex();
-    
-            return 'sign='+ sign + '&' + 'sign_type=' + 'MD5&' + signParamsString;
-        }
-
-        const keyStr = signature()
-
-
-        axios.get('/product/activity_list?' + keyStr)
-        .then(function (response) {
-            console.log(response);
-        })
-        .catch(function (error) {
-
-        });
         
         this.login             = this.login.bind(this)
         this.handleSubmit      = this.handleSubmit.bind(this)
