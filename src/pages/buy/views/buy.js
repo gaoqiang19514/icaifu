36ccc691bc33e1bba46387fe81522e9270b7d7fd
gaoqiang19514 <gaoqiang19514@163.com>
2018-08-30 20:20:18 +0800

    update

--- src/pages/buy/views/buy.js
+++ src/pages/buy/views/buy.js
@@ -10,19 +10,28 @@ import 'boxicons'
 import { buildBuyAuthSign } from '@/api/api.js';
 import { AnimateFullLayer } from '@/common/animateLayer';
 
-import { LayoutFixedBottom, LayoutFixedSibling, LayoutBoxBet, LayoutBoxBetCenter, LayoutBoxWrapSec, LayoutBoxWrap, LayoutPrimaryBox, LayoutBoxVerticalCenter } from '@/common/commonStyled';
-import { createDataList } from '@/util';
-
+import { LayoutFixedBottom, LayoutFixedSibling, LayoutBoxBet, button, LayoutFlexBox, LayoutBoxBetCenter, LayoutBoxWrapSec, LayoutBoxWrap, LayoutPrimaryBox, LayoutBoxVerticalCenter } from '@/common/commonStyled';
 
 import service_icon from './images/service_icon.png';
-import arrow_icon from './images/arrow_icon.png';
-import checkbox_ok from './images/checkbox_icon_ok.png';
-import checkbox_no from './images/checkbox_icon_no.png';
 import circle_ok from './images/circle_check_icon_ok.png';
 import circle_no from './images/circle_check_icon_no.png';
 
-import styles from './style.scss';
-import './layer.css';
+const LayoutAside = styled.div`
+    flex: 1;
+    background: #fff;
+`;
+
+const LayoutMain = styled.div`
+    flex-basis: 7.8667rem;
+    display: flex;
+`;
+
+const LayoutAsideBox = styled.div`
+    color: #898996;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+`;
 
 const StyleGreyText = styled.div`
     color: #898996;
@@ -36,15 +45,153 @@ const StyleRow = styled.div`
     background: #fff;
 `;
 
-const StyleInputWrap = LayoutBoxBetCenter.extend`
+const StyleInputPart = LayoutBoxBetCenter.extend`
     padding: .4rem;
     margin-bottom: .4rem;
     background-color: #fff3f0;
+
+    label {
+        font-size: 0.6667rem;
+        margin-right: 0.2667rem;
+    }
+
+    input {
+        color: #1a1b1c;
+        padding: 0;
+        outline: none;
+        border: 0;
+        border-radius: 0;
+        width: 100%;
+        font-size: 0.6667rem;
+        background: transparent;
+        line-height: normal;
+    }
+
+    button {
+        align-self: stretch;
+        margin-left: 0.2667rem;
+        flex-shrink: 0;
+        padding: 0;
+        color: #f8695e;
+        width: auto;
+        border: 0;
+        outline: none;
+        background: transparent;
+    }
 `;
 
 const StyleHeadBox = LayoutBoxWrap.extend`
     font-size: 0.4rem;
     background: #fff;
+`;
+
+const StyleFullButton = button.extend`
+    font-size: 0.4267rem;
+    height: 1.3333rem;
+    line-height: 1.3333rem;
+    flex: 1;
+    color: #fff;
+    background-image: linear-gradient(90deg, #f94c50 0%, #f77366 100%), linear-gradient(#d7d7d7, #d7d7d7);
+`;
+
+const StyleServiceIcon = styled.img`
+    margin: 0.2rem auto 0.1333rem auto;
+    width: 0.5333rem;
+    height: 0.52rem;
+`;
+
+const StyleCouponWrap = {
+    "padding": "0.4rem 0.4rem 1px 0.4rem"
+}
+
+const StyleCoupon = styled.div`
+    display: flex;
+    margin-bottom: 0.4rem;
+    box-shadow: 0rem 0.0267rem 0.08rem 0rem rgba(0, 0, 0, 0.08);
+`;
+
+const StyleCouponAside = styled.div`
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    color: #fff;
+    width: 3.5rem;
+    padding: 0.4rem;
+    border-radius: 0.1067rem 0rem 0rem 0.1067rem;
+    background-image: linear-gradient(90deg, #f94c50 0%, #f77366 100%), linear-gradient( #f8695e, #f8695e);
+`;
+
+const StyleCouponMain = styled.div`
+    flex-grow: 1;
+    background: #fff;
+    border-radius: 0rem 0.1067rem 0.1067rem 0rem ;
+`;
+
+const StyleCouponAmount = styled.div`
+    margin-bottom: 0.2667rem;
+    font-size: 0.6667rem;
+`;
+
+const StyleCouponCondition = styled.div`
+    font-size: 0.3467rem;
+`;
+
+const StyleCouponHd = styled.div`
+    position: relative;
+    display: flex;
+    align-items: flex-end;
+    padding: 0.4rem 0.4rem 0 0.4rem;
+    margin-bottom: 0.2667rem;
+`;
+
+const StyleCouponBd = styled.div`
+    font-size: 10px;
+    line-height: 0.48rem;
+    padding: 0 0.4rem 0.2rem 0.4rem;
+`;
+
+const StyleCouponFt = styled.div`
+    position: relative;
+    text-align: right;
+    padding: 0.2rem 0.4rem;
+`;
+
+const StyleCouponTitle = styled.span`
+    font-weight: bold;
+    font-size: 0.48rem;
+`;
+
+const StyleCouponBadge = styled.span`
+    margin-left: 0.2667rem;
+    color: #fff;
+    border-radius: 0.2rem;
+    padding: 0.0667rem 0.1333rem;
+    background-image: linear-gradient(90deg, #f94c50 0%, #f77366 100%), linear-gradient( #f8695e, #f8695e);
+`;
+
+const StyleCouponExpire = styled.span`
+    color: #898996;
+    font-size: 0.2933rem;
+`;
+
+const StyleCouponCheckbox = styled.span`
+    position: absolute;
+    top: 0.4rem;
+    right: 0.4rem;
+    width: 0.4rem;
+    height: 0.4rem;
+`;
+
+const StyleButton = button.extend`
+    width: 100%;
+    color: #fff;
+    font-size: 0.4267rem;
+    height: 1.3333rem;
+    line-height: 1.3333rem;
+    background-image: linear-gradient(90deg, #f94c50 0%, #f77366 100%), linear-gradient( #f7645b, #f7645b);
+    border-radius: 0.6667rem;
+    box-shadow: 0rem 0.0267rem 0.1067rem 0rem rgba(249,82,83,0.75);
 `;
 
 class Coupon extends Component {
@@ -92,7 +239,7 @@ class Coupon extends Component {
         }
 
         return(
-            <div className="coupon-list">
+            <div style={ StyleCouponWrap }>
                 <TransitionGroup>
                     {
                         coupons.map((item) => {
@@ -103,30 +250,30 @@ class Coupon extends Component {
                                     appear={ true }
                                     timeout={ 500 }
                                 >
-                                    <div className={ styles.coupon }>
-                                        <div className={ styles.coupon_aside }>
-                                            <div className={ styles.coupon_amount }>￥15000</div>
-                                            <div className={ styles.coupon_condition }>满20000元可用</div>
-                                        </div>
-                                        <div className={ styles.coupon_main }>
-                                            <div className={ styles.coupon_hd }>
-                                                <span onClick={ () => onSelectCouponHandle(id) } className={ styles.coupon_checkbox }>
+                                    <StyleCoupon>
+                                        <StyleCouponAside>
+                                            <StyleCouponAmount>￥15000</StyleCouponAmount>
+                                            <StyleCouponCondition>满20000元可用</StyleCouponCondition>
+                                        </StyleCouponAside>
+                                        <StyleCouponMain>
+
+                                            <StyleCouponHd>
+                                                <StyleCouponCheckbox>
                                                     <img src={ checkIcon } alt="勾选"/>
-                                                </span>
-                                                <span className={ styles.coupon_title }>代金券</span>
-                                                <span className={ styles.coupon_badge }>今天过期</span>
-                                            </div>
-                                            <div className={ styles.coupon_bd }>
+                                                </StyleCouponCheckbox>
+                                                <StyleCouponTitle>代金券</StyleCouponTitle>
+                                                <StyleCouponBadge>今天过期</StyleCouponBadge>
+                                            </StyleCouponHd>
+                                            <StyleCouponBd>
                                                 <div>投资期限=30天或60天</div>
                                                 <div>除极计划外其余产品可用</div>
-                                            </div>
-                                            <div className={ styles.coupon_ft }>
-                                                <span className={ styles.coupon_expire }>
-                                                有效期至 2018-12-31
-                                                </span>
-                                            </div>
-                                        </div>
-                                    </div>
+                                            </StyleCouponBd>
+                                            <StyleCouponFt>
+                                                <StyleCouponExpire>有效期至 2018-12-31</StyleCouponExpire>
+                                            </StyleCouponFt>
+
+                                        </StyleCouponMain>
+                                    </StyleCoupon>
                                 </CSSTransition>
                             )
                         })
@@ -191,7 +338,7 @@ export default class extends Component {
                 <AnimateFullLayer flag={ couponFlag }>
                     <Coupon />
                     <ButtonFixed>
-                        <button className="button" onClick={ this.toggleCouponHandle }>确定</button>
+                        <StyleButton onClick={ this.toggleCouponHandle }>确定</StyleButton>
                     </ButtonFixed>
                 </AnimateFullLayer>
 
@@ -214,11 +361,11 @@ export default class extends Component {
                             </LayoutBoxBet>
                         </LayoutBoxWrapSec>
 
-                        <StyleInputWrap>
-                            <label className={ styles.m_label }>￥</label>
-                            <input onChange={ this.changeHandle } className={ styles.m_input } value={ investAmount } type="text" placeholder="100元起投"/>
-                            <button onClick={ this.investBalance } className={ styles.m_button }>余额全投</button>
-                        </StyleInputWrap>
+                        <StyleInputPart>
+                            <label>￥</label>
+                            <input onChange={ this.changeHandle } value={ investAmount } type="text" placeholder="100元起投"/>
+                            <button onClick={ this.investBalance }>余额全投</button>
+                        </StyleInputPart>
 
                         <StyleGreyText>
                             <LayoutBoxBet>
@@ -270,13 +417,17 @@ export default class extends Component {
                 <div>
                     <LayoutFixedSibling/>
                     <LayoutFixedBottom>
-                        <div className={styles.m_op}>
-                            <div className={styles.m_op__service}>
-                                <img className={styles.m_op__service_icon} src={service_icon} alt="客服图标"/>
-                                <div>客服</div>
-                            </div>
-                            <button onClick={ this.submitHandle } className={styles.m_op__invest} to="/">余额不足，请充值14251元</button>
-                        </div>
+                        <LayoutFlexBox>
+                            <LayoutAside>
+                                <LayoutAsideBox>
+                                    <StyleServiceIcon src={ service_icon } alt="客服图标"/>
+                                    <div>客服</div>
+                                </LayoutAsideBox>
+                            </LayoutAside>
+                            <LayoutMain>
+                                <StyleFullButton>余额不足，请充值14251元</StyleFullButton>
+                            </LayoutMain>
+                        </LayoutFlexBox>
                     </LayoutFixedBottom>
                 </div>
 
