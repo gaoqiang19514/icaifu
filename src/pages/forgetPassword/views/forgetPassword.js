5a0e16e22d7025f069b4cf3a94984f0fd25961e4
gaoqiang19514 <gaoqiang19514@163.com>
2018-08-30 21:52:58 +0800

    update

--- src/pages/forgetPassword/views/forgetPassword.js
+++ src/pages/forgetPassword/views/forgetPassword.js
@@ -1,40 +1,91 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import styled from 'styled-components'
 
-import style from './style.scss'
+import { button, input } from "@/common/commonStyled"
+
+const LayoutContainer = styled.div`
+`;
+
+const LayoutBox = styled.div`
+    display: flex;
+    padding: 0.4rem;
+`;
+
+const LayoutButtonBox = styled.div`
+    padding: 0.4rem;
+`;
+
+const LayoutBoxWrap = styled.div`
+    background: #fff;
+`;
+
+const StyleButton = button.extend`
+    width: 100%;
+    color: #fff;
+    padding: 0.4rem;
+    border-radius: 3px;
+    font-weight: bold;
+    -webkit-letter-spacing: .2em;
+    -moz-letter-spacing: .2em;
+    -ms-letter-spacing: .2em;
+    letter-spacing: .2em;
+    background-image: linear-gradient(90deg,#f94c50 0%,#f77366 100%),linear-gradient( #f7645b,#f7645b);
+    box-shadow: 0rem 0.0267rem 0.1067rem 0rem rgba(249,82,83,0.75);
+`;
+
+const StyleMinButton = button.extend`
+    color: #fff;
+    padding: 0.4rem;
+    border-radius: 3px;
+    font-weight: bold;
+    -webkit-letter-spacing: .2em;
+    -moz-letter-spacing: .2em;
+    -ms-letter-spacing: .2em;
+    letter-spacing: .2em;
+    background-image: linear-gradient(90deg,#f94c50 0%,#f77366 100%),linear-gradient( #f7645b,#f7645b);
+    box-shadow: 0rem 0.0267rem 0.1067rem 0rem rgba(249,82,83,0.75);
+`;
+
+const StyleImg = styled.img`
+`;
+
+const StyleInput = input.extend`
+    flex: 1;
+    font-size: 0.4rem;
+    padding: 0.1333rem;
+`;
+
+
 
 export default () => {
     return (
-        <div className={style.container}>
+        <LayoutContainer>
             
-            <div className={style.box}>
-                <div className={style.line}>
-                    <input type="text" placeholder="请输入手机号" />
-                </div>
+            <LayoutBoxWrap>
+                <LayoutBox>
+                    <StyleInput type="text" placeholder="请输入手机号" />
+                </LayoutBox>
+                <LayoutBox>
+                    <StyleInput type="text" placeholder="请输入右侧的验证码" />
+                    <StyleImg src="http://192.168.1.25/ver_code.jpg?id=0.6439735326867758" alt=""/>
+                </LayoutBox>
+                <LayoutBox>
+                    <StyleInput type="text" placeholder="请输入短信验证码" />
+                    <StyleMinButton>获取验证码</StyleMinButton>
+                </LayoutBox>
+                <LayoutBox>
+                    <StyleInput type="text" placeholder="请输入新密码" />
+                </LayoutBox>
+                <LayoutBox>
+                    <StyleInput type="text" placeholder="请再次输入新密码" />
+                </LayoutBox>
+            </LayoutBoxWrap>
 
-                <div className={style.line}>
-                    <input type="text" placeholder="请输入右侧的验证码" />
-                    <img className={style.img} src="http://192.168.1.25/ver_code.jpg?id=0.6439735326867758" alt=""/>
-                </div>
+            <LayoutButtonBox>
+                <StyleButton>下一步</StyleButton>
+            </LayoutButtonBox>
 
-                <div className={style.line}>
-                    <input type="text" placeholder="请输入短信验证码" />
-                    <span className={style.link}>获取验证码</span>
-                </div>
-
-                <div className={style.line}>
-                    <input type="text" placeholder="请输入新密码" />
-                </div>
-
-                <div className={style.line}>
-                    <input type="text" placeholder="请再次输入新密码" />
-                </div>
-            </div>
-
-            <div className={style.wrap}>
-                <button className={style.button}>下一步</button>
-            </div>
-
-        </div>
+        </LayoutContainer>
     )
 }
